30b40227a71353e713df6b8b48031e90b489ecd4
Elizaveta <elizabess27@mail.ru>
2024-01-15 01:17:49 +0300

    ПЕрвое занятие готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,24 @@
 function getArrayParams(...arr) {
+  
+  let min, max, sum, avg;
 
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if( arr[i] < min){
+      min = arr[i];
+    }
+    if (arr[i] > max){
+      max = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
+  
   return { min: min, max: max, avg: avg };
 }
 
