a6633b36949cbc39f000011f64099275ece7ad27
Elizaveta <elizabess27@mail.ru>
2024-02-21 21:37:02 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,42 @@
-function Student(name, gender, age) {
+function Student(name, gender, age) { //функция конструктор самого студента
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  let student1 = new Student("Василиса", "женский", 19)
+  let student2 = new Student("Артем", "мужской", 25)
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.setSubject = function (subjectName) { //добавляет поле предмет к студенту
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.addMarks = function (marks) {
+    if(this.marks === undefined){
+      this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
+  Student.prototype.addMarks = function (...marks) { //добавляет оценки к студенту
+      if(this.marks === undefined){
+        this.marks = marks;
+      } else {
+        this.marks = this.marks.concat(marks);
+      }
+    }
   
-}
+  Student.prototype.getAverage = function () { //среднее арифметическое оценок
+    if(this.marks === undefined){
+      return 'Нет оценок';
+    } else {
+      return this.marks.reduce((a, v) => a+v, 0) / this.marks.length;
+    }
+  }
+  
+  Student.prototype.exclude = function (reason) { //удаляет студента
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
