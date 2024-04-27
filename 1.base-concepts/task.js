9b7f5be1aba850737fa727fbdfbbe2672219a23b
Elizaveta <elizabess27@mail.ru>
2024-04-27 17:47:12 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+   const discriminant = b ** 2 - 4 * a * c;
+   if (discriminant < 0) {
+    return [];
+   }
+
+   else if (discriminant === 0) {
+    const root = - b / (2 * a);
+    return [root];
+   }
+
+   else {
+    const root1 = ((- b + Math.sqrt(discriminant)) / 2 * a);
+    const root2 = ((- b - Math.sqrt(discriminant)) / 2 * a);
+    return [root1, root2];
+   }
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof percent != "number" || typeof contribution !== "number" || typeof amount !== "number" || typeof countMonths !== "number") {
+    return ("Ошибка ввода данных");
+  }
+
+  if (percent < 0 || contribution < 0 || amount < 0 || countMonths < 0) {
+    return ("Введите положительные числа");
+  }
+
+  const monthlyInterestRate = percent / 100 / 12;
+  const loanAmount = amount - contribution;
+  const monthlyPayment = loanAmount * (monthlyInterestRate + monthlyInterestRate / ((1 + monthlyInterestRate) ** countMonths - 1));
+  const totalPayment = monthlyPayment * countMonths;
+
+  return parseFloat(totalPayment.toFixed(2));
 }
+
