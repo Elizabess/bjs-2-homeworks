18713fc81f0044d6305d7125aa85b142587bc8da
Elizaveta <elizabess27@mail.ru>
2024-05-22 15:35:31 +0300

    Пятое задание готово ( без задачи со *)

---
+++ 5.classes/task.js
@@ -0,0 +1,61 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+    }
+  
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+}
+
+class Library {
+    constructor(name) {
+      this.name = name;
+      this.books = [];
+    }
+  
+    addBook(book) {
+      if (book.state > 30) {
+        this.books.push(book);
+      }
+    }
+  
+    findBookBy(type, value) {
+      for (let book of this.books) {
+        if (book[type] === value) {
+          return book;
+        }
+      }
+      return null;
+    }
+  
+    giveBookByName(bookName) {
+      for (let i = 0; i < this.books.length; i++) {
+        if (this.books[i].name === bookName) {
+          const book = this.books[i];
+          this.books.splice(i, 1);
+          return book;
+        }
+      }
+      return null;
+    }
+}
+
