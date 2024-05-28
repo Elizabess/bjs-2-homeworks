537f637bbff6e4b97f18ce81bef274b784f0dcd2
Elizaveta <elizabess27@mail.ru>
2024-05-28 15:10:10 +0300

    Пятое задание окончательно исправлено

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -34,7 +34,7 @@ class Magazine extends PrintEditionItem {
 }
 
 class Book extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, author) {
+  constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
     this.author = author;
     this.type = "book";
@@ -42,22 +42,22 @@ class Book extends PrintEditionItem {
 }
 
 class NovelBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
-    super(name, releaseDate, pagesCount);
+  constructor(author ,name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "novel";
   }
 }
 
 class FantasticBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "fantastic";
   }
 }
 
-class DetectivBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
-    super(name, releaseDate, pagesCount);
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "detective";
   }
 }
@@ -75,15 +75,23 @@ class Library {
   }
   
   findBookBy(type, value) {
-    const findResult = this.books.find((item) => item[type] === value);
-    return findResult;
+    for (let book of this.books) {
+      if (book[type] === value) {
+        return book;
+      }
+    }
+    return null;
   }
   
   giveBookByName(bookName) {
-    const book = this.findBookBy("name", bookName);
-    if (!book) return null;
-    this.books = this.books.filter((item) => item.name !== bookName);
-    return book;
+    for (let i = 0; i < this.books.length; i++) {
+      if(this.books[i].name === bookName) {
+        const book = this.books[i];
+        this.books.splice(i, 1);
+        return book;
+      }
+    }
+    return null;
   }
 }
 
